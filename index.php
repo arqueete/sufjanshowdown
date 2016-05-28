b3e3fabd84acf5f215a0f13b7a3d59fc55638b51
Jessica Gleason <jessica@jessicagleason.com>
2016-05-28 18:18:42 -0500

    Getting song titles improvement, without check for duplicates

--- index.php
+++ index.php
@@ -7,16 +7,6 @@
 		<?php 
 			include 'dbconnect.php';
 			
-			/*function db_query($query) {
-				
-				$result = mysqli_query($mysqli,$query);
-				
-				return $result;
-			}
-			
-			
-			$songs = db_query("SELECT `id` from `songs` WHERE active=1");*/
-			
 			function db_query($query) {
 				// Connect to the database
 				$connection = db_connect();
@@ -26,13 +16,10 @@
 
 				return $result;
 			}
-			
-			//$connection = db_connect();
-			//$songs = mysqli_query($connection,"SELECT `id` from `songs` WHERE active=1");
-			
+
 			/* Get IDs of all active songs */
 			function getSongs() {
-				$songs = db_query("SELECT `id` from `songs` WHERE active=1");
+				$songs = db_query("SELECT id from songs WHERE active=1");
 
 				$ids = array();
 				$row = mysqli_fetch_assoc($songs);
@@ -56,7 +43,7 @@
 				//echo "<br />";
 				
 				//get the title of the song at that ID
-				$song = db_query("SELECT `title` from `songs` WHERE id='$randomID'" );
+				$song = db_query("SELECT * from songs WHERE id='$randomID'" );
 				$songInfo = mysqli_fetch_assoc($song);
 				//$songTitle = $songInfo;
 				
@@ -68,54 +55,24 @@
 				$ids = getSongs();
 				$left = getRandomSong($ids);
 				$right = getRandomSong($ids);
-				if ($check < 3) {
-					if ($right[title] === $left[title]) {
-						echo "oops same song try again";
-						echo "<br />";
-						$check++;
-						getRandomSongs();
-						//getTheSongs($check);
-					} else {
-						echo "good to go";
-						echo "<br />";
-						echo $right[title];
-						echo "<br />";
-						echo $left[title];
-					}
-				} else {
-					echo "tried too many times";
-				}
+				$randomSongs = array();
+				$randomSongs[] = $left;
+				$randomSongs[] = $right;
+				
+				return $randomSongs;
 			}
 			
-			getRandomSongs();
-				
-				
+			$randomSongs = getRandomSongs();
+			echo "<br />";
+			$left = $randomSongs[0];
+			$right = $randomSongs[1];
 			
-			/*$check = 0;
-			function getTheSongs($check) {
-				$left = getRandomSong($ids);
-				echo "<br />";
-				echo $left;
-				echo "<br />";
-				$right = getRandomSong($ids);
-				echo "<br />";
-				echo $right;
-				echo "<br />";
-				if ($check > 3) {
-					if ($right === $left) {
-						echo "oops same song try again";
-						$check++;
-						//getTheSongs($check);
-					} else {
-						echo "good to go";
-					}
-				} else {
-					echo "tried too many times";
-				}
+			echo $left[title];
+			echo "<br />";
+			echo $right[title];
 			
-			}
+			//print_r array_values($randomSongs);
 			
-			getTheSongs();*/
 			
 		?>
 	</body>
