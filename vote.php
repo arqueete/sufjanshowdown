aa947dfebfd66b51659b869c62edc1b1c3449232
Jessica Gleason <jessica@jessicagleason.com>
2016-05-28 22:25:42 -0500

    Checking which song in the game was the winner

--- vote.php
+++ vote.php
@@ -17,7 +17,7 @@
 	//get submitted values
 	$gameID = $_POST['gameID'];
 	$vote = $_POST['song'];
-	
+
 	//get the ids of all games
 	$games = db_query("SELECT id FROM games");
 	$ids = array();
@@ -41,10 +41,26 @@
 			echo $songInfo[title];
 			echo "<br />";
 			echo $songInfo[rating];
+			echo "<br />";
+			foreach ($theseSongsInfo as $key=>$value) {
+				echo $value;
+				echo "<br />";
+				if ($value == $vote) {
+					echo "this is the winning song";
+					echo "<br />";
+				} else {
+					echo "this is the losing song";
+					echo "<br />";
+				}
+			}
 			
 			$winnerSet = db_query("UPDATE `games` SET winner='$vote' WHERE id=$gameID");
 			
-		
+			//time to do some math and figure out the new scores
+			
+			/*WinProbability = 1/(10^(( Opponent’s Current Rating–Player’s Current Rating)/400) + 1)
+			ScoringPt = 1 point if they win the match, 0 if they lose, and 0.5 for a draw.
+			Player’s New Rating = Player’s Old Rating + (K-Value * (ScoringPt–Player’s Win Probability))*/
 			
 			
 		} else {
