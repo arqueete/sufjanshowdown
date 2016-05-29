62d716df7448dc8552f7fbb87c85398d03305725
Jessica Gleason <jessica@jessicagleason.com>
2016-05-28 21:48:47 -0500

    Check if game is valid and that song is present in that game

--- vote.php
+++ vote.php
@@ -15,23 +15,33 @@
 	}
 	$gameID = $_POST['gameID'];
 	$vote = $_POST['song'];
-	$thisGame = db_query("SELECT id from games");
+	$thisGame = db_query("SELECT id FROM games");
 	$ids = array();
-	$row = mysqli_fetch_assoc($thisGame);
+	//$row = mysqli_fetch_assoc($thisGame);
 	$ids = array();
 	while ($row = mysqli_fetch_assoc($thisGame)) 
 	{
 		$ids[] = $row[id];
 	}
-	echo $gameID;
-	echo "<br />";
-	if (in_array($gameID,$ids)) {
-		echo "good to go";
+	//echo $gameID;
+	//echo "<br />";
+	if (in_array($gameID,$ids,$vote)) {
+		//now check that song chosen is part of that game
+		$theseSongs = db_query("SELECT `left`,`right` FROM `games` where id=$gameID");
+		$theseSongsInfo = mysqli_fetch_assoc($theseSongs);
+		//echo "<br />";
+		//print_r (array_values($theseSongsInfo));
+		if (in_array($vote,$theseSongsInfo)) {
+			//song and game are valid
+		} else {
+			echo "song error";
+		}
+		//$row = mysqli_fetch_assoc($thisSong);
 	} else {
 		echo "something wrong";
 	}
 	echo "<br />";
-	print_r (array_values($ids));
+	//print_r (array_values($ids));
 	
 	$song = db_query("SELECT * from songs WHERE id='$vote'" );
 	$songInfo = mysqli_fetch_assoc($song);
